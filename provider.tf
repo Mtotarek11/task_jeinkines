046dec7fc6db4a60e849518f5d597768097c7b5c
Omran <86138549+Mtotarek11@users.noreply.github.com>
2026-09-12 11:36:08 +0300

    Update AWS provider credentials and endpoints

--- provider.tf
+++ provider.tf
@@ -1,13 +1,12 @@
 provider "aws" {
-  region     = "us-east-1"
-  access_key = "test"
-  secret_key = "test"
-
+  region                      = "us-east-1"
+  access_key                  = "mock_access_key"
+  secret_key                  = "mock_secret_key"
   skip_credentials_validation = true
-  skip_requesting_account_id  = true
   skip_metadata_api_check     = true
+  skip_requesting_account_id  = true
 
   endpoints {
-    ec2 = "http://localhost:4566"
+    ec2 = "http://host.docker.internal:4566"
   }
 }
